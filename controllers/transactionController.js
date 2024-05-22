409b2fe92977c61472dc2030495aec46a2dff804
Mat <matt.tan@aiqueinnovation.com>
2024-05-22 16:15:46 +0800

    Fixed: filter get all transaction based on the user loggedIn

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,7 +2,10 @@ const catchAsync = require('../utils/catchAsync');
 const Transaction = require('./../models/transactionModel');
 
 exports.getAllTransaction = catchAsync(async (req, res, next) => {
-  const transactions = await Transaction.find();
+  const userId = req.user._id; // Get userId from the request object (populated by protect middleware)
+
+  // Find transactions for the specific user
+  const transactions = await Transaction.find({ userId });
 
   // SEND RESPONSE
   res.status(200).json({
@@ -13,6 +16,26 @@ exports.getAllTransaction = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+// exports.getAllTransaction = catchAsync(async (req, res, next) => {
+//   const user = await User.findById(req.user.id);
+
+//   console.log('UserId', user);
+
+//   const transactions = await Transaction.find({ user });
+
+//   if (!user) {
+//     return next(new AppError('No user found with this ID', 404));
+//   }
+
+//   res.status(200).json({
+//     status: 'success',
+//     results: transactions.length,
+//     data: {
+//       transactions
+//     }
+//   });
+// });
 
 exports.createTransaction = catchAsync(async (req, res, next) => {
   try {
